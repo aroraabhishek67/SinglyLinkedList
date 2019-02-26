4d559303d0fd026b1c455e199046e852848c3aa8
aroraabhishek67 <aroraabhishek67@gmail.com>
2019-02-26 14:26:19 +0530

    add constructors

--- src/main/SinglyLinkedList.java
+++ src/main/SinglyLinkedList.java
@@ -5,6 +5,11 @@ public class SinglyLinkedList {
         private int data;
         private Node next;
 
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+
         public int getData() {
             return data;
         }
